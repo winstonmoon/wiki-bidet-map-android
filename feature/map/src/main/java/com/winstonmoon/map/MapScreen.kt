3861d6eca94fcfc9cea36a258cc8729d53de08ad
Sungjun Moon <project_m_@hotmail.com>
2023-04-18 23:53:09 +0900

    add search app bar in progress

--- feature/map/src/main/java/com/winstonmoon/map/MapScreen.kt
+++ feature/map/src/main/java/com/winstonmoon/map/MapScreen.kt
@@ -1,7 +1,17 @@
 package com.winstonmoon.map
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.unit.dp
+import com.google.android.material.shape.CornerSize
+import kotlin.math.round
+import androidx.compose.ui.unit.dp
 
 @Composable
 internal fun MapRoute(
@@ -16,5 +26,14 @@ internal fun MapRoute(
 internal fun MapScreen(
     modifier: Modifier = Modifier,
 ) {
+    Box(
+        modifier =
+            modifier.shadow(shadowElevation, shape, clip = false)
+            .then(if (border != null) Modifier.border(border, shape) else Modifier)
+            .background(color = backgroundColor, shape = RoundedCornerShape(corner = CornerSize(8.dp)))
+            .clip(shape)
+    ) {
+
+    }
 
 }
