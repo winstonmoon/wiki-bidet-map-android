556dade2a2de2b700d1d0e1ff6a550e6145f0ed1
Sungjun Moon <project_m_@hotmail.com>
2024-02-10 23:56:12 +0900

    fix indent

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,9 +13,9 @@ dependencyResolutionManagement {
         // Mapbox Maven repository
         maven {
             url = uri("https://api.mapbox.com/downloads/v2/releases/maven")
-// Do not change the username below. It should always be "mapbox" (not your username).
+            // Do not change the username below. It should always be "mapbox" (not your username).
             credentials.username = "mapbox"
-// Use the secret token stored in gradle.properties as the password
+            // Use the secret token stored in gradle.properties as the password
             credentials.password = providers.gradleProperty("MAPBOX_DOWNLOADS_TOKEN").get()
             authentication.create<BasicAuthentication>("basic")
         }
